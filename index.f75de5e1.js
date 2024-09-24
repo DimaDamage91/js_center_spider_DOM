dec8026bda3363780c29a1a001c37aa9fc047530
Korshun <dmitriy.kor91@gmail.com>
2024-09-24 17:01:30 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,8 @@
+"use strict";
+// write code here
+const spiderMan = document.querySelector(".spider");
+spiderMan.style.top = "50%";
+spiderMan.style.left = "50%";
+spiderMan.style.transform = "translate(-50%, -50%)";
+
+//# sourceMappingURL=index.f75de5e1.js.map
